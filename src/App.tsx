be9c4f418872b3a9387beac923f67956140436c4
Saiyam Yadav <ydvsaiyam001@gmail.com>
2024-11-16 13:44:05 +0530

    Setup initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { Button } from "@/components/ui/button";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Loading from "./components/Loader";
 
-export default function Home() {
+const Home = lazy(() => import("./pages/Home"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Login = lazy(() => import("./pages/Login"));
+
+const App = () => {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <Router>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
-}
+};
+export default App;
